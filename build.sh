5435436036fc98ee4cd17bd85f5981dbba1b1b44
SaiRaghavArramSema4 <sairaghav.arram@sema4genomics.com>
2018-05-17 12:52:51 +0530

    updated build file

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ setup(){
 	  	  curl -L -b "oraclelicense=a" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz | tar -xzC $BASE_HOME
 	fi
 	
-	if [ ! -d "./apache-maven-3.5.2" ]; then 
-	  curl -sL http://www-eu.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz | tar -xzC $BASE_HOME
+	if [ ! -d "./apache-maven-3.0.5" ]; then 
+	  curl -sL http://www-eu.apache.org/dist/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz | tar -xzC $BASE_HOME
 	fi
 
-	export M2_HOME=$BASE_HOME/apache-maven-3.5.2/
+	export M2_HOME=$BASE_HOME/apache-maven-3.0.5/
 	export M2=$M2_HOME/bin
 	export JAVA_HOME=$BASE_HOME/jdk1.8.0_131
 	export PATH=$PATH:$JAVA_HOME/bin/
